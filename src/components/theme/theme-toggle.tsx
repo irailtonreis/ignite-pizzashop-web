c78f16fc60b7a527c314ff8648b687b3421766ac
irailtonreis <irailton.oliveira91@gmail.com>
2024-01-27 11:42:45 -0300

    feat: Perfil no cabeçalho

--- src/components/theme/theme-toggle.tsx
+++ src/components/theme/theme-toggle.tsx
@@ -1,12 +1,12 @@
 import { Moon, Sun } from 'lucide-react'
 
+import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from '@/components/dropdown-menu'
-import { Button } from '@/components/ui/button'
+} from '@/components/ui/dropdown-menu'
 
 import { useTheme } from './theme-provider'
 
